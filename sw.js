c5c1a5fa1ecf3499e5416eb17aa9b2c8734674d2
mustafakaraapca-collab <mustafakaraapca@icloud.com>
2026-08-24 02:12:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="oto-yikama-pro-v16-1";
+const CACHE="oto-yikama-pro-v17";
 const ASSETS=["./","./index.html","./randevu.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{

82cc06dc635ff4b5cb173717fc3672cfca8f64ed
mustafakaraapca-collab <mustafakaraapca@icloud.com>
2026-08-23 21:21:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE="oto-yikama-ciro-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+  if(request.mode==="navigate"){
+    event.respondWith(
+      fetch(request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(request).then(r=>r||caches.match("./index.html")))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(request).then(cached=>cached||fetch(request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(request,copy));
+      return response;
+    }))
+  );
+});
